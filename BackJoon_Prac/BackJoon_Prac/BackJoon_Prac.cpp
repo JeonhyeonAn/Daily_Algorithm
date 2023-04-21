8e918b58655799b97bb09e6ad7385c17849743b0
JeonghyeonAn <86709249+JeonhyeonAn@users.noreply.github.com>
2023-04-21 11:38:50 +0900

    0001
    백준_개수 세기

---
+++ BackJoon_Prac/BackJoon_Prac/BackJoon_Prac.cpp
@@ -0,0 +1,36 @@
+﻿// BackJoon_Prac.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+	vector<int> arr;
+	int n;
+	cin >> n;
+
+	int x;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		arr.push_back(x);
+	}
+
+	int value;
+	cin >> value;
+
+	int count = 0;
+	for (auto it : arr)
+	{
+		if (it == value)
+			count++;
+	}
+
+	cout << count;
+
+}
