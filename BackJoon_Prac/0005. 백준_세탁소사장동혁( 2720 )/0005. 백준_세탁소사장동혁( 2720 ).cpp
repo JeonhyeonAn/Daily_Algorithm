1053d2fa9dde889be2d0570dba681acafaa8460c
JeonghyeonAn <86709249+JeonhyeonAn@users.noreply.github.com>
2023-05-02 15:43:02 +0900

    0005
    0005. 백준_세탁소사장동혁( 2720 )

---
+++ BackJoon_Prac/0005. 백준_세탁소사장동혁( 2720 )/0005. 백준_세탁소사장동혁( 2720 ).cpp
@@ -0,0 +1,56 @@
+﻿// 0005. 백준_세탁소사장동혁( 2720 ).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+ 
+
+	int Exchange = 0;
+	int Q = 25, D = 10, N = 5, P = 1;
+	int Cycle = 0;
+
+	cin >> Cycle;
+
+	vector<int> ExchangeArr(Cycle);
+
+	for (int i = 0; i < Cycle; i++)
+	{
+		cin >> ExchangeArr[i];
+	}
+	
+
+	for (int value : ExchangeArr)
+	{
+		Exchange = value;
+
+		cout << Exchange / Q << " ";
+		Exchange = Exchange % Q;
+
+		cout << Exchange / D << " ";
+		Exchange = Exchange % D;
+
+		cout << Exchange / N << " ";
+		Exchange = Exchange % N;
+
+		cout << Exchange / P << " ";
+		Exchange = Exchange % P;
+
+		cout << endl;
+	}
+
+
+
+
+
+
+
+
+}
+
---
+++ BackJoon_Prac/0006. 백준_중앙 이동 알고리즘 ( 2903 )/0006. 백준_중앙 이동 알고리즘 ( 2903 ).cpp
@@ -0,0 +1,10 @@
+﻿// 0006. 백준_중앙 이동 알고리즘 ( 2903 ).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
