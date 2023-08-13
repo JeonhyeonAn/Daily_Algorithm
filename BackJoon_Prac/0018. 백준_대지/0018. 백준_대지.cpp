cc608ed86613224f8dc92b49c4a0523e95027e73
JeonghyeonAn <86709249+JeonhyeonAn@users.noreply.github.com>
2023-08-14 01:34:55 +0900

    0018

---
+++ BackJoon_Prac/0018. 백준_대지/0018. 백준_대지.cpp
@@ -0,0 +1,53 @@
+﻿// 0018. 백준_대지.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	int N;
+	cin >> N;
+	
+	//vector<unsigned int> Arr;
+	int Xmax, Xmin, Ymax, Ymin;
+	bool bDoOnce = false;
+
+	for (int i = 0; i < N; i++)
+	{
+		if (N <= 1)
+		{
+			cout << "0";
+			return 0;
+		}
+
+		int TempX, TempY;
+		cin >> TempX >> TempY;
+
+		if (!bDoOnce)
+		{
+			Xmax = TempX;
+			Xmin = TempX;
+			Ymax = TempY;
+			Ymin = TempY;
+
+			bDoOnce = true;
+		}
+
+		if (TempX > Xmax)
+			Xmax = TempX;
+		else if (TempX < Xmin)
+			Xmin = TempX;
+
+		if (TempY > Ymax)
+			Ymax = TempY;
+		else if (TempY < Ymin)
+			Ymin = TempY;
+	}
+
+	auto Result = (Xmax - Xmin) * (Ymax - Ymin);
+	cout << Result;
+}
---
+++ BackJoon_Prac/0019. 백준_삼각형 외우기 (10101)/0019. 백준_삼각형 외우기 (10101).cpp
@@ -0,0 +1,37 @@
+﻿// 0019. 백준_삼각형 외우기 (10101).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    while (true)
+    {
+
+    int A, B, C;
+
+    cin >> A >> B >> C;
+
+    if (A == 60 && B == 60 && C == 60 )
+    {
+        cout << "Equilateral" << endl;
+        return 0;
+    }
+
+    if (A + B + C == 180)
+    {
+        if (A != B && A != C && B != C)
+            cout << "Scalene" << endl;
+        else
+            cout << "Isosceles" << endl;
+    }
+    else
+        cout << "Error" << endl;
+    
+	}
+
+}
+
